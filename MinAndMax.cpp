58f3a098338644da30a2aa2f72697e1902f51e02
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-11-24 10:06:24 +0530

    finding min and max ele in arrary

---
+++ MinAndMax.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void rand(vector<int> &arr, int n) {
+    for (int i = 0; i < n; i++) arr[i] = (rand() % 100) + 1;
+}
+
+void printArray(vector<int> &arr, int n) {
+    for (int i = 0; i < n; i++) cout << arr[i] << " "; cout << endl;
+}
+
+int main() {
+    int size = 10;
+    vector<int> arr(10);
+
+    // add rand ele into the array
+    rand(arr, size);
+    // print elements 
+    printArray(arr, size);
+    // find the min and max in array
+    int mn = INT_MAX, mx = INT_MIN;
+    for (int i = 0; i < size; i++) {
+        mn = min(mn, arr[i]);
+        mx = max(mx, arr[i]);
+    }
+
+    cout << "Min ele : " << mn << endl;
+    cout << "Max ele : " << mx << endl;
+}
