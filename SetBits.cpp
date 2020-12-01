1fa0e7ebe10ce85768a6cf49c15342b077fdcadb
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-12-01 23:14:51 +0530

    set bits added

---
+++ SetBits.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    srand(time(NULL));
+    int num  = rand() % 50;
+    cout << "Num : " << num << endl;
+
+    int setBits = 0;
+
+    // int n = (int)(log(num) / log(2));
+    // cout << "log N : " << n << endl;
+
+    // for (int i = 0; i <= n; i++) {
+    //     if(num & (1 << i)) setBits++;
+    // }
+    while(num) {
+        setBits += (num & 1);
+        num >>= 1;
+    }
+
+    cout << "setBits : " << setBits << endl;
+
+
+
+}
