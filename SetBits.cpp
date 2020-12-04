a85c426226e20f945679575143d68b64cd5c1f8b
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-12-04 22:47:23 +0530

    set bits modified

--- SetBits.cpp
+++ SetBits.cpp
@@ -2,24 +2,28 @@
 using namespace std;
 
 int main() {
-    srand(time(NULL));
-    int num  = rand() % 50;
-    cout << "Num : " << num << endl;
+    // srand(time(NULL));
+    // int num  = rand() % 50;
+    // cout << "Num : " << num << endl;
 
-    int setBits = 0;
+    // int setBits = 0;
 
-    // int n = (int)(log(num) / log(2));
-    // cout << "log N : " << n << endl;
+    // // int n = (int)(log(num) / log(2));
+    // // cout << "log N : " << n << endl;
 
-    // for (int i = 0; i <= n; i++) {
-    //     if(num & (1 << i)) setBits++;
+    // // for (int i = 0; i <= n; i++) {
+    // //     if(num & (1 << i)) setBits++;
+    // // }
+    // while(num) {
+    //     setBits += (num & 1);
+    //     num >>= 1;
     // }
-    while(num) {
-        setBits += (num & 1);
-        num >>= 1;
-    }
 
-    cout << "setBits : " << setBits << endl;
+    // cout << "setBits : " << setBits << endl;
+
+    // cout << "===============" << endl;
+
+    for (int i = 0; i < 20; i++) cout << i << "==> " << __builtin_popcount(i) << endl;
 
 
 
